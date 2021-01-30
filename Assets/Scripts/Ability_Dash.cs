a0158ce113ddb10578634f5070fb68c1853e24f4
Felix Szczesny <felix@maschinen-mensch.com>
2021-01-30 19:44:12 +0100

    added Dash

---
+++ Assets/Scripts/Ability_Dash.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ability_Dash : MonoBehaviour
+{
+
+    [SerializeField] float dashForce;
+    [SerializeField] float recoverTime;
+    bool playerCanDash;
+    bool pressDashButton = false;
+    float time;
+    Rigidbody rigidbody;
+
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        playerCanDash = true;
+        time = 0;
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Dash"))
+        {
+            pressDashButton = true;
+        }
+
+        Recover();
+    }
+
+    private void FixedUpdate()
+    {
+        Dash();
+    }
+
+    void Dash()
+    {
+        if (pressDashButton && playerCanDash)
+        {
+            Vector3 dashDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            rigidbody.AddForce(dashDirection * dashForce, ForceMode.Impulse);
+            playerCanDash = false;
+            time = 0;
+            pressDashButton = false;
+        }
+    }
+
+    void Recover()
+    {
+        time += Time.deltaTime;
+        if (time >= recoverTime)
+        {
+            playerCanDash = true;
+        }
+    }
+}
