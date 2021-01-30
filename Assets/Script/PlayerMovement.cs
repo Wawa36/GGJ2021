1a888e5a02ee20f67f193e93255c583e8313fba9
Felix Szczesny <felix@maschinen-mensch.com>
2021-01-30 11:47:41 +0100

    added basic scene and player movement script

---
+++ Assets/Script/PlayerMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] float speed;
+    Rigidbody rigidbody;
+
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        rigidbody.MovePosition(rigidbody.position 
+            + (transform.forward * Input.GetAxis("Vertical") * speed * Time.fixedDeltaTime) 
+            + (transform.right * Input.GetAxis("Horizontal") * speed * Time.fixedDeltaTime));
+    }
+}
