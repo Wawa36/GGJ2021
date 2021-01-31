ab79fc61775bac63d41fc61588d44eda3f750f2e
Felix Szczesny <felix@maschinen-mensch.com>
2021-01-31 12:14:24 +0100

    Added Fireball

---
+++ Assets/Scripts/LookAtCamera.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtCamera : MonoBehaviour
+{
+    void Start()
+    {
+        this.transform.rotation = Quaternion.AngleAxis(this.transform.rotation.eulerAngles.y, -Camera.main.transform.forward) 
+                                * Quaternion.AngleAxis(- Camera.main.transform.rotation.eulerAngles.x, Vector3.right);
+    }
+}
